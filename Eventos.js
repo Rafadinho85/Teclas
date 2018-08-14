30e3e96a82a36c59365e2c767d9831f3fb3777e7
Rafadinho <rafa85jogo@gmail.com>
2018-08-14 10:07:26 -0600

    agregar la funcion para detectar teclado

---
+++ Eventos.js
@@ -0,0 +1,33 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento)
+{
+switch (evento.keyCode) {
+  case teclas.UP:
+    console.log("arriba");
+    break;
+  case teclas.DOWN:
+    console.log("abajo");
+    break;
+  case teclas.LEFT:
+    console.log("IZQUIERDA");
+    break;
+  case teclas.RIGHT:
+    console.log("RIGHT");
+    break;
+
+    default:
+    console.log("otra tecla");
+
+}
+
+}
