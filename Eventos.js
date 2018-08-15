962d93303dbdd094753d47c8934495435705d3ed
Rafadinho <rafa85jogo@gmail.com>
2018-08-15 10:37:18 -0600

    agregando metodo dibujar linea

--- Eventos.js
+++ Eventos.js
@@ -9,6 +9,22 @@ console.log(teclas);
 
 document.addEventListener("keyup", dibujarTeclado);
 
+var cuadrito = document.getElementById("areaDibujo");
+var papelito =  cuadrito.getContext("2d");
+
+dibujarLinea("red", 100, 100, 200, 200, papelito);
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
 function dibujarTeclado(evento)
 {
 switch (evento.keyCode) {
